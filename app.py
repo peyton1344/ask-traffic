42d2196dc54e22732b07b0fbc348601352bc2381
peyton1344 <151428091+peyton1344@users.noreply.github.com>
2024-07-03 11:51:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def traffic_thailand():
     r = palm.chat(**model, messages=q)
     return(render_template("traffic_thailand.html",r=r.last))
 
+@app.route("/traffic_singapore", methods=["GET","POST"])
+def traffic_thailand():
+    q = "singapore traffic"
+    r = palm.chat(**model, messages=q)
+    return(render_template("traffic_singapore.html",r=r.last))
+
 if __name__ == "__main__":
     app.run()
-    
