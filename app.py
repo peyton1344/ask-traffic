b62f62952c1fe8f6feeae20b850f148a9bc32226
peyton1344 <151428091+peyton1344@users.noreply.github.com>
2024-07-03 10:39:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request
+import google.generativeai as palm
+import os
+
+api_key = os.getenv("MAKERSUIT_API_KEY")
+palm.configure(api_key=api_key)
+
+model = {"model": "models/chat-bison-001"}
 
 app = Flask(__name__)
 
@@ -6,5 +13,17 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main", methods=["GET","POST"])
+def main():
+    r = request.form.get("q")
+    print(r)
+    return(render_template("main.html",r=r))
+
+@app.route("/traffic_thailand", methods=["GET","POST"])
+def traffic_thailand():
+    q = "thailand traffic"
+    r = palm.chat(**model, messages=q)
+    return(render_template("traffic_thailand.html",r=r.last))
+
 if __name__ == "__main__":
-    app.run()
+app.run()
